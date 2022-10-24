1d6d348ca0803a5a9ddd170b86e4468a5b3f5783
Piotr <piotr.szczypka@vp.pl>
2022-10-24 11:39:48 +0200

    add page templates

--- src/api/homepage/routes/homepage.js
+++ src/api/homepage/routes/homepage.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about router
+ * homepage router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::about.about');
+module.exports = createCoreRouter('api::homepage.homepage');
