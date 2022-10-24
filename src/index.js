d1d0afb3a08a79e5b2ed6ab50ea03f6d33b8ad96
Piotr <piotr.szczypka@vp.pl>
2022-10-24 16:19:04 +0200

    edit mail

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports = {
   /**
    * An asynchronous register function that runs before
    * your application is initialized.
-   *
    * This gives you an opportunity to extend code.
    */
   register(/*{ strapi }*/) {},
@@ -12,7 +11,6 @@ module.exports = {
   /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
-   *
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
