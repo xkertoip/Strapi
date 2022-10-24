721a841eb3a541dc1aa0b71a22966b5b950ceaff
Piotr <piotr.szczypka@vp.pl>
2022-10-24 15:28:46 +0200

    edit email config

--- config/plugins.js
+++ config/plugins.js
@@ -11,8 +11,8 @@ module.exports = ({ env }) => ({
           api_key: env('SENDGRID_API_KEY'),
         },
         settings: {
-          defaultFrom: 'p.szczypka.dev@gmail.com',
-          defaultReplyTo: 'p.szczypka.dev@gmail.com'
+          defaultFrom: env('MAIL_FROM'),
+          defaultReplyTo: env('MAIL_REPLAY_TO')
         }
       }
     },
